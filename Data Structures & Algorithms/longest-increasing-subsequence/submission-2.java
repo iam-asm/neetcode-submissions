8e77d90b77a545e90f28561c8d25bf1dfa2c0da5
iam-asm <158086589+iam-asm@users.noreply.github.com>
2026-08-10 19:46:23 +0530

    Add: longest-increasing-subsequence - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max = 1;
+
+        for (int i=1; i < nums.length; i++) {
+            for (int j = i-1; j >= 0; j--) {
+                if (nums[i] > nums[j] && dp[j]+1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            if (dp[i] >= max) max = dp[i];
+        }
+
+        return max;
+    }
+}
